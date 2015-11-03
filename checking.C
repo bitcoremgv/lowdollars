e19970ef54da77e03d63d58096b297fa583df6c2
crypto dollars for climate change <climatechangecryptodollars@gmx.com>
2015-11-02 21:33:53 -0500

    checking sending
    dhecking sending

---
+++ checking.C
@@ -0,0 +1,46 @@
+#include  
+
+the db package
+
+the dbs 
+
+the dbs have the ledgers
+
+wallets ledger				have 	cryptos used 	crypteds	lowdollars 
+checkers ledger				have	send backs		asks		id			checks 
+constancy ledger			have	constancy checks			unconstancy issues		constancy asks
+rewards asks ledger			have	checkers		rewards asks
+mean asked rewards ledger	have	mean ask
+
+
+and the 
+
+checkers used guide		have	checkers			needs		processes
+wallets used guide		have	wallets	needs		processes
+cryptos used guide		have	cryptos				needs		processes 
+dbs used guide			have	db					needs		processes
+datas udsed guide		have	datas	needs	ux
+
+
+the 
+
+ledger used guide			have	ledgers	dbpackage	processes 
+checking used guide			have	checkingpackage		processes
+send used guide				have	sendpackage			processes
+rwards asks limits guide	have	needs				limits		
+rewards guide				have	needs				processes
+
+
+
+
+
+wallets are unlimited 
+and the elctricity needed to unwanted decrypt for it should avoid it
+this should limit 
+
+and it shuold be reencrypted or used
+
+
+
+
+  
